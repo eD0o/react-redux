0f66840c8912d1bc5fdae211c702b21347ecb065
eduardo.benigno15@gmail.com <eduardo.benigno15@gmail.com>
2023-11-21 16:36:51 -0300

    feat: cap 4

--- 0301_currying/030102_example-without-currying-2/script.js
+++
@@ -1,7 +0,0 @@
-const li = Array.from(document.querySelectorAll('li'));
-
-const dataSlideList = li.map((el) => el.getAttribute('data-slide'));
-const idList = li.map((el) => el.getAttribute('id'));
-
-console.log(dataSlideList);
-console.log(idList);
