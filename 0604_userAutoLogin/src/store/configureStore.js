820056f9602ae8d4ecf226b77731f42163e65851
eduardo.benigno15@gmail.com <eduardo.benigno15@gmail.com>
2024-01-22 12:10:54 -0300

    feat: preparing class 0604

---
+++ 0604_userAutoLogin/src/store/configureStore.js
@@ -0,0 +1,11 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import photo from './photo';
+import token from './token';
+import user from './user';
+
+// const middleware = (getDefaultMiddleware) => getDefaultMiddleware()
+
+const reducer = combineReducers({ photo, token, user })
+const store = configureStore({ reducer })
+
+export default store;
