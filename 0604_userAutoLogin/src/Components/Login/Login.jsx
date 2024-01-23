469a0033a1556aa167e8286d7bab28ef3464cab1
eduardo.benigno15@gmail.com <eduardo.benigno15@gmail.com>
2024-01-22 21:53:59 -0300

    fix: 0604 until 12min

--- 0604_userAutoLogin/src/Components/Login/Login.jsx
+++ 0604_userAutoLogin/src/Components/Login/Login.jsx
@@ -4,15 +4,16 @@ import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
-import { UserContext } from '../../UserContext';
 import styles from './Login.module.css';
 import NotFound from '../NotFound';
 import { useSelector } from 'react-redux';
+import Loading from '../Helper/Loading';
 
 const Login = () => {
-  const { login } = React.useContext(UserContext);
 
-  const { data } = useSelector(state => state.user)
+  const { data, loading } = useSelector(state => state.user)
+
+  if (loading) return <Loading />
 
   if (data) return <Navigate to="/conta" />;
   return (
--- 0604_userAutoLogin/src/Components/User/UserHeaderNav.jsx
+++ 0604_userAutoLogin/src/Components/User/UserHeaderNav.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
-import { UserContext } from '../../UserContext';
 import MinhasFotos from '../../assets/feed.svg?react';
 import Estatisticas from '../../assets/estatisticas.svg?react';
 import AdicionarFoto from '../../assets/adicionar.svg?react';
 import Sair from '../../assets/sair.svg?react';
 import styles from './UserHeaderNav.module.css';
 import useMedia from '../../Hooks/useMedia';
+import { useDispatch } from 'react-redux';
+import { userLogout } from '../../store/user';
 
 const UserHeaderNav = () => {
-  const { userLogout } = React.useContext(UserContext);
+  const dispatch = useDispatch()
   const mobile = useMedia('(max-width: 40rem)');
   const [mobileMenu, setMobileMenu] = React.useState(false);
   const navigate = useNavigate();
@@ -20,7 +21,7 @@ const UserHeaderNav = () => {
   }, [pathname]);
 
   function handleLogout() {
-    userLogout();
+    dispatch(userLogout());
     navigate('/login');
   }
 
@@ -29,17 +30,15 @@ const UserHeaderNav = () => {
       {mobile && (
         <button
           aria-label="Menu"
-          className={`${styles.mobileButton} ${
-            mobileMenu && styles.mobileButtonActive
-          }`}
+          className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive
+            }`}
           onClick={() => setMobileMenu(!mobileMenu)}
         ></button>
       )}
 
       <nav
-        className={`${mobile ? styles.navMobile : styles.nav} ${
-          mobileMenu && styles.navMobileActive
-        }`}
+        className={`${mobile ? styles.navMobile : styles.nav} ${mobileMenu && styles.navMobileActive
+          }`}
       >
         <NavLink to="/conta" end>
           <MinhasFotos />
