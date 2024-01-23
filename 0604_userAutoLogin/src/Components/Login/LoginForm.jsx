727a7d7afec5f3f291f1ffdbbfc35e1d6fe3b888
eduardo.benigno15@gmail.com <eduardo.benigno15@gmail.com>
2024-01-22 22:03:13 -0300

    feat: class 0604

--- 0604_userAutoLogin/src/Components/Login/LoginForm.jsx
+++ 0604_userAutoLogin/src/Components/Login/LoginForm.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
-import { UserContext } from '../../UserContext';
 import Error from '../Helper/Error';
 import styles from './LoginForm.module.css';
 import stylesBtn from '../Forms/Button.module.css';
