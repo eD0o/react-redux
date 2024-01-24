c481a4dd0b9e4604636c34e40cf15498978ade4a
eduardo.benigno15@gmail.com <eduardo.benigno15@gmail.com>
2024-01-24 16:07:42 -0300

    feat: class 0606

---
+++ 0606_modal/src/store/configureStore.js
@@ -0,0 +1,13 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import photo from './photo';
+import token from './token';
+import user from './user';
+import feed from './feed';
+import ui from './ui';
+
+// const middleware = (getDefaultMiddleware) => getDefaultMiddleware()
+
+const reducer = combineReducers({ photo, token, user, feed, ui })
+const store = configureStore({ reducer })
+
+export default store;
